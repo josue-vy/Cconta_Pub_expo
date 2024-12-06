fc6422fddd7725ff529922c5ce963f43d2e52d10
josue-vy <josvillanuevay@upt.pe>
2024-12-06 17:05:21 -0500

    cconta-pubb-v0.1.5

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,11 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@': './',
-            '@/components': './components',
-            '@/constants': './constants',
-            '@/hooks': './hooks',
-          },
-        },
-      ],
-    ],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    ['module-resolver', {
+      root: ['./'],
+      alias: {
+        '@': './',
+      }
+    }]
+  ]
 };
